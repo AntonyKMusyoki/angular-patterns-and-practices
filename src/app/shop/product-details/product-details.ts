0f9260b3a313cbc4de737428e92d0217a159abbb
Antony Kyalo Musyoki <antony.kyalo@bookinghub.com>
2026-03-28 21:10:45 -0500

    Ch04/Angular Load and Runtime Performance Best Practices/Chrome DevTools Performance Tab: Diagnosing App App Performance - LCP requrst discovery

--- src/app/shop/product-details/product-details.ts
+++ src/app/shop/product-details/product-details.ts
@@ -1,15 +1,18 @@
 import { Component, input } from '@angular/core';
 import { IProduct } from '../product.model';
-import { CurrencyPipe, NgClass } from '@angular/common';
+import { CurrencyPipe, NgClass, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'bot-product-details',
-  imports: [CurrencyPipe, NgClass],
+  imports: [CurrencyPipe, NgClass, NgOptimizedImage],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css'
 })
 export class ProductDetails {
   product = input.required<IProduct>();
+  //Add input property for parent catalog to set image priority
+  //priority  -> (property) ProductDetails.priority: InputSignal<boolean>
+  priority = input(false);
 
   getImageUrl(product: IProduct) {
     return '/images/robot-parts/' + product.imageName;
